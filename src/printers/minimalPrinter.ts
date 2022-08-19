76997dea7e40ca7fbe24265e9aec0f6cbff976fc
adlacruzes <adlacruzes@gmail.com>
2022-08-19 20:20:02 +0200

    Fix minimal format output

--- src/printers/minimalPrinter.ts
+++ src/printers/minimalPrinter.ts
@@ -2,9 +2,9 @@ import {PrinterInterface} from "./printerInterface";
 
 export class MinimalPrinter implements PrinterInterface {
   print(rows: any[]): void {
+    console.log('Missing foreign keys index')
+    console.log();
     for (let row of rows) {
-      console.log('Missing foreign keys index')
-      console.log();
       console.log(` - ${row.table}.${row.columns}`)
       console.log();
     }
